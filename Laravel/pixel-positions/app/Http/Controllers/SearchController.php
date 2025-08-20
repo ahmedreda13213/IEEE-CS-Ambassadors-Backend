cea8f8c84d95b105576415f80fe9e8977c668741
ahmedreda13213 <ahmed.reda13213@gmail.com>
2025-08-20 23:22:55 +0300

    Laravel

---
+++ Laravel/pixel-positions/app/Http/Controllers/SearchController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function __invoke(){
+       $jobs = Job::query()->with(['employer','tag'])->where('title','LIKE','%',request('q').'%');
+       return view  ('results', ['jobs' => $jobs]);
+    }
+}
